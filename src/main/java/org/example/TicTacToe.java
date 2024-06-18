9858ef9deb01d6770a8629f68afec605567681f2
Haris Beshta Bradaric <haris.bradaric44@gmail.com>
2024-06-18 16:05:05 +0200

    UPDATE: Kommentare für bessere Verständlichkeit wurden hinzugefügt. In Zeile 26 wurde ein  System.out.println(); hinzugefügt für bessere Lesbarkeit

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,13 +1,13 @@
 package org.example;
 
 public class TicTacToe {
-    private char[][] spielbrett;
+    private char[][] spielbrett;// Das Spielfeld für das TicTacToe-Spiel
 
     public TicTacToe(){
-        spielbrett = new char[3][3];
-        spielbrettHerstellen();
+        spielbrett = new char[3][3]; // Initialisiert ein 3x3 Spielfeld
+        spielbrettHerstellen(); // Initialisiert ein 3x3 Spielfeld
     }
-
+    // Methode zum Initialisieren des Spielfeldes
     private void spielbrettHerstellen(){
         for (int zeile = 0; zeile < 3; zeile++) {
             for (int spalte = 0; spalte < 3; spalte++) {
@@ -15,14 +15,15 @@ public class TicTacToe {
             }
         }
     }
-
+    // Setzt jedes Feld auf '-'
     public void spielbrettAnzeigen(){
         for (int zeile = 0; zeile < 3; zeile++) {
             for (int spalte = 0; spalte < 3; spalte++) {
-                System.out.print(spielbrett[zeile][spalte]+" ");
+                System.out.print(spielbrett[zeile][spalte]+" "); // Druckt jedes Feld mit einem Leerzeichen dazwischen
             }
-            System.out.println();
+            System.out.println();// Neue Zeile nach jeder Reihe
         }
+        System.out.println(); // Zusätzliche Leerzeile für bessere Lesbarkeit
     }
 
 
