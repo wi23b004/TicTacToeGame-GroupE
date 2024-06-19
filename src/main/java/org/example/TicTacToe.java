92cdf780e915cc1ebfc49f91ae2b031fd3cd61e0
Haris Beshta Bradaric <haris.bradaric44@gmail.com>
2024-06-19 16:54:22 +0200

    UPDATE: Checkstyle-Konfiguration und JUnit-Abhängigkeiten aktualisiert

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,30 +1,30 @@
 package org.example;
 
 public class TicTacToe {
-    private char[][] spielbrett;// Das Spielfeld für das TicTacToe-Spiel
+    private char[][] spielbrett; // Das Spielfeld für das TicTacToe-Spiel
 
-    public TicTacToe(){
+    public TicTacToe() {
         spielbrett = new char[3][3]; // Initialisiert ein 3x3 Spielfeld
         spielbrettHerstellen(); // Initialisiert ein 3x3 Spielfeld
     }
+
     // Methode zum Initialisieren des Spielfeldes
-    private void spielbrettHerstellen(){
+    private void spielbrettHerstellen() {
         for (int zeile = 0; zeile < 3; zeile++) {
             for (int spalte = 0; spalte < 3; spalte++) {
                 spielbrett[zeile][spalte] = '-';
             }
         }
     }
+
     // Setzt jedes Feld auf '-'
-    public void spielbrettAnzeigen(){
+    public void spielbrettAnzeigen() {
         for (int zeile = 0; zeile < 3; zeile++) {
             for (int spalte = 0; spalte < 3; spalte++) {
-                System.out.print(spielbrett[zeile][spalte]+" "); // Druckt jedes Feld mit einem Leerzeichen dazwischen
+                System.out.print(spielbrett[zeile][spalte] + " "); // Druckt jedes Feld mit einem Leerzeichen dazwischen
             }
-            System.out.println();// Neue Zeile nach jeder Reihe
+            System.out.println(); // Neue Zeile nach jeder Reihe
         }
         System.out.println(); // Zusätzliche Leerzeile für bessere Lesbarkeit
     }
-
-
 }
