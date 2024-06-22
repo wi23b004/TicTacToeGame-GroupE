9527519dc542de1c76632d64ca1dad173d1eff51
Haris Beshta Bradaric <haris.bradaric44@gmail.com>
2024-06-22 14:42:44 +0200

    ADD: ersten zwei Test für die neue Methode zugMachen, welche die gültigkeit/ungültigkeit eines Zuges testen wurden implementiert und haben erfolgreich bestanden

--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -21,6 +21,21 @@ public class TicTacToeTest {
         TicTacToe spiel = new TicTacToe();
         assertDoesNotThrow(() -> spiel.spielbrettAnzeigen());
     }
+
+    @Test
+    public void testZugMachenGueltigePosition() {
+        TicTacToe spiel = new TicTacToe();
+        boolean ergebnis = spiel.zugMachen(1,1,'x');
+        assertTrue(ergebnis);
+        assertEquals('X', spiel.getSpielBrett()[1][1]);
+    }
+
+    @Test
+    public void testZugMachenUngueltigePosition() {
+        TicTacToe spiel = new TicTacToe();
+        boolean ergebnis = spiel.zugMachen(3,7,'x');
+        assertFalse(ergebnis);
+    }
 }
 
 
