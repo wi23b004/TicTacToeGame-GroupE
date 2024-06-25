b9248b8e9f9493eba04cdfa1a4f1ea8b650b35a8
Emre Tuncer <you@example.com>
2024-06-25 23:13:14 +0200

    ADD: Erstellen einer neuen Klasse und beginnen mit der ersten Implematation in dem Branch feature/board-class

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = '-';
+            }
+        }
+    }
+    public void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
