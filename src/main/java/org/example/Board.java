d97cf4f3ae7f6fcc25cf2e6df854ded38db65ebf
Emre Tuncer <you@example.com>
2024-06-25 23:14:58 +0200

    ADD: Implematation in der Klasse Board fertigstellen und in dem Branch feature/board-class committen

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -7,6 +7,30 @@ public class Board {
         cells = new char[3][3];
         clear();
     }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == '-';
+    }
+
+    public boolean place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public void clear() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -14,6 +38,7 @@ public class Board {
             }
         }
     }
+
     public void print() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
