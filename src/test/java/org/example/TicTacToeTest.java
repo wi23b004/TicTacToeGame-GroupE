3d9e9fc28d098876ad2bcec77acc09c250e5963d
Emre Tuncer <you@example.com>
2024-06-25 17:18:18 +0200

    UPDATE: Branch Konflikt lösen

--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class TicTacToeTest {
 
     @Test
@@ -21,4 +22,34 @@ public class TicTacToeTest {
         assertDoesNotThrow(() -> spiel.spielbrettAnzeigen());
     }
 
+    @Test
+    public void testZugMachenGueltigePosition() {
+        TicTacToe spiel = new TicTacToe();
+        boolean ergebnis = spiel.zugMachen(1,1,'x');
+        assertTrue(ergebnis);
+        assertEquals('X', spiel.getSpielBrett()[1][1]);
+    }
+
+    @Test
+    public void testZugMachenUngueltigePosition() {
+        TicTacToe spiel = new TicTacToe();
+        boolean ergebnis = spiel.zugMachen(3,7,'x');
+        assertFalse(ergebnis);
+    }
+
+    @Test
+    public void testZugMachenUngueltigesSymbol() {
+        TicTacToe spiel = new TicTacToe();
+        boolean ergebnis = spiel.zugMachen(1,1,'a');
+        assertFalse(ergebnis);
+    }
+
+    @Test
+    public void testZugMachenFeldBesetzt() {
+        TicTacToe spiel = new TicTacToe();
+        spiel.zugMachen(1,1,'x');
+        boolean ergebnis = spiel.zugMachen(1,1,'o');
+        assertFalse(ergebnis);
+        assertEquals('X', spiel.getSpielBrett()[1][1]);
+    }
 }
