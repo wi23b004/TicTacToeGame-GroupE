967327f13c3e7ec8c2d4397e9ab97923c1ca2ead
Elyesa Bilge <wi23b106@technikum-wien.at>
2024-06-29 21:18:07 +0200

    ADD: Neue Unit Tests für Player-Klasse hinzugefügt, da die Checks auf GitHub fehlgeschlagen sind.
    - Test für die Initialisierung des Players mit Marker 'X'
    - Test für die Initialisierung des Players mit Marker 'O'
    - Test zur Sicherstellung, dass der Marker nicht leer ist

--- src/test/java/org/example/PlayerTest.java
+++ src/test/java/org/example/PlayerTest.java
@@ -1,18 +1,25 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
+
     @Test
-    public void testGetMarker_Positive() {
+    public void testPlayerInitialization() {
         Player player = new Player('X');
         assertEquals('X', player.getMarker());
     }
 
     @Test
-    public void testGetMarker_Negative() {
+    public void testPlayerInitializationWithDifferentMarker() {
+        Player player = new Player('O');
+        assertEquals('O', player.getMarker());
+    }
+
+    @Test
+    public void testPlayerMarkerIsNotEmpty() {
         Player player = new Player('X');
-        assertNotEquals('O', player.getMarker());
+        assertNotEquals('\0', player.getMarker());
     }
 }
